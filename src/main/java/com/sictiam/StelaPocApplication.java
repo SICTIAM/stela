31010575ff04103e57f256d01913b3138396da85
Benoit Orihuela <bobeal@gmail.com>
2017-03-21 10:52:15 +0100

    Initial commit with bootstrapped app from Spring Initializr

---
+++ src/main/java/com/sictiam/StelaPocApplication.java
@@ -0,0 +1,12 @@
+package com.sictiam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StelaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StelaPocApplication.class, args);
+	}
+}
