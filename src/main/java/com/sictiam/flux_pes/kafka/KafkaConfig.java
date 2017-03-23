4c03219eabc686ac486b7acea070454c58718632
Benoit Orihuela <bobeal@gmail.com>
2017-03-23 11:54:06 +0100

    [Evo] ref #545 - Set a default group id

--- src/main/java/com/sictiam/flux_pes/kafka/KafkaConfig.java
+++ src/main/java/com/sictiam/flux_pes/kafka/KafkaConfig.java
@@ -38,6 +38,7 @@ public class KafkaConfig {
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
