56997be56935b8c69e39e1dcaab109b8b46656c9
Benoit Orihuela <bobeal@gmail.com>
2017-03-24 15:48:48 +0100

    [Fix] ref #549 - Persist a message in DB

--- src/main/java/com/sictiam/flux_pes/model/PesObjet.java
+++ src/main/java/com/sictiam/flux_pes/model/PesObjet.java
@@ -4,19 +4,18 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-/**
- * Created by s.vergon on 24/03/2017.
- */
+
 @Entity
 public class PesObjet {
+
     @Id
-    //@GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer id;
     private String titre;
 
-    //protected PesObjet() {}
+    protected PesObjet() {}
 
-    public PesObjet(Integer id,String titre) {
+    public PesObjet(Integer id, String titre) {
         this.id = id;
         this.titre = titre;
     }
--- src/main/java/com/sictiam/flux_pes/service/PesService.java
+++ src/main/java/com/sictiam/flux_pes/service/PesService.java
@@ -1,15 +1,13 @@
 package com.sictiam.flux_pes.service;
 
 import com.sictiam.flux_pes.model.PesObjet;
-import com.sictiam.flux_pes.repository.PesObjRepositery;
+import com.sictiam.flux_pes.repository.PesRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 
 @Service
 public class PesService {
@@ -18,28 +16,25 @@ public class PesService {
 
     @Autowired
     private KafkaTemplate kafkaTemplate;
-    private PesObjRepositery repopes;
+
+    @Autowired
+    private PesRepository pesRepository;
+
     public void sendTestMessage(String message) {
         LOGGER.warn("Sending test message to Kafka");
         kafkaTemplate.send("test-topic", message);
-    }
-
-    public void storepes(PesObjet pesobj) {
-        pesobj.toString();
-        repopes.findAll();
     }
 
     @KafkaListener(topics = "test-topic")
     public void receiveTestMessage(String message) {
 
         LOGGER.warn("Got a message : {}", message);
-        PesObjet objpes;
-        objpes = new PesObjet(1,message);
+        PesObjet objpes = new PesObjet(1, message);
         LOGGER.warn("Objet pes à créer : {}", objpes);
-        storepes(objpes);
+        //storepes(objpes);
         //PesObjRepositery repopes;
         //sauvegarde en base de données
-        //repopes.save(objpes);
+        pesRepository.save(objpes);
         //repopes.findAll();
     }
 }
