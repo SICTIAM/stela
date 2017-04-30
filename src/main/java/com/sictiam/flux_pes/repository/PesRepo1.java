913366b63faaa3af25bb1719157e8fce5b7642d1
Benoit Orihuela <borihuela@ozwillo.org>
2017-04-30 19:31:44 +0200

    Remove previous, now unused, PES module

--- src/main/java/com/sictiam/flux_pes/repository/PesRepo1.java
+++
@@ -1,9 +0,0 @@
-package com.sictiam.flux_pes.repository;
-
-import com.sictiam.flux_pes.model.Pes;
-import com.sictiam.flux_pes.model.PesObjet;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PesRepo1 extends CrudRepository<PesObjet,Long> {
-
-}
--- src/main/java/com/sictiam/flux_pes/repository/PesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sictiam.flux_pes.repository;
-
-import com.sictiam.flux_pes.model.Pes;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PesRepository extends CrudRepository<Pes,String> {
-
-}
