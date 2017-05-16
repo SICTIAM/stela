4d85e8b54a3a68ac1779de2d74484767bb515807
Franck Boucher <boucher.fk@gmail.com>
2017-05-16 16:57:42 +0200

    [Evo] ref #868 - Bootstrap Front End architecture

---
+++ api-gateway/src/main/resources/jsx/Miat.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Miat extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Liste des Actes</h1>
+            </div>
+        )
+    }
+}
+
+export default Miat
