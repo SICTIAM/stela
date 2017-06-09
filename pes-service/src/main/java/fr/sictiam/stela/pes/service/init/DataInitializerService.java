0fe90fdb96d707eda007314bbcae9cccc332dfdb
Franck Boucher <boucher.fk@gmail.com>
2017-06-09 11:04:28 +0200

    [Evo] ref #1043 - Replace args by profile to trigger the clean/import

--- pes-service/src/main/java/fr/sictiam/stela/pes/service/init/DataInitializerService.java
+++ pes-service/src/main/java/fr/sictiam/stela/pes/service/init/DataInitializerService.java
@@ -1,42 +1,33 @@
 package fr.sictiam.stela.pes.service.init;
 
-import fr.sictiam.stela.pes.dao.DaoService;
 import fr.sictiam.stela.pes.model.Pes;
 import fr.sictiam.stela.pes.service.PesService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("dev")
+@Profile("clean-db")
 public class DataInitializerService implements ApplicationListener<ApplicationReadyEvent> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DataInitializerService.class);
 
-    private final ApplicationArguments args;
     private final PesService pesService;
-    private final DaoService daoService;
 
     @Autowired
-    public DataInitializerService(ApplicationArguments args, PesService pesService, DaoService daoService) {
+    public DataInitializerService(PesService pesService) {
 
-        this.args = args;
         this.pesService = pesService;
-        this.daoService = daoService;
     }
 
     @Override
     public void onApplicationEvent(final ApplicationReadyEvent event) {
-        if (args.containsOption("reset-db")) {
-            LOGGER.info("Data reset asked");
-            daoService.cleanDb();
-            importData();
-        }
+        LOGGER.info("Data reset asked");
+        importData();
     }
 
     private void importData() {
