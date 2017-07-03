293bbde8d90d8ba3b94ddf2c311e735943cdb8f8
yann <yann.prosper@gmail.com>
2017-07-03 11:07:18 +0200

    REST annotation modification.
    Signed-off-by: yann <yann.prosper@gmail.com>

--- acte-service/src/main/java/fr/sictiam/stela/acteservice/controller/ActeRestController.java
+++ acte-service/src/main/java/fr/sictiam/stela/acteservice/controller/ActeRestController.java
@@ -6,10 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -27,13 +28,13 @@ public class ActeRestController {
         this.repository = repository;
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public ResponseEntity<List<Acte>> readActes() {
         List<Acte> actes = this.repository.findAll();
         return new ResponseEntity<List<Acte>>(actes, HttpStatus.OK);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/id/{id}")
+    @GetMapping("/id/{id}")
     public ResponseEntity<Acte> getById(@PathVariable Long id) {
         ResponseEntity<Acte> response;
 
@@ -47,7 +48,7 @@ public class ActeRestController {
         return response;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/numero/{numero}")
+    @GetMapping("/numero/{numero}")
     public ResponseEntity<Acte> getByNumero(@PathVariable String numero) {
         ResponseEntity<Acte> response;
 
@@ -61,7 +62,7 @@ public class ActeRestController {
         return response;
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     ResponseEntity<?> create(@RequestBody Acte acte) {
         Acte result = this.repository.save(acte);
         URI location = ServletUriComponentsBuilder
