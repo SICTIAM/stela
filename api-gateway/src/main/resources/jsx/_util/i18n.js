2013127c1ee1a479053656aa121117ca248b457f
Franck Boucher <boucher.fk@gmail.com>
2017-08-31 11:47:10 +0200

    [Fix] fix #1733 - Fix i18n json retrieval

--- api-gateway/src/main/resources/jsx/_util/i18n.js
+++ api-gateway/src/main/resources/jsx/_util/i18n.js
@@ -24,10 +24,10 @@ i18n
             // returning a path: 
             // function(lngs, namespaces) { return customPath; } 
             // the returned path will interpolate lng, ns if provided like giving a static path 
-            loadPath: '../locales/{{lng}}/{{ns}}.json',
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
 
             // path to post missing resources 
-            addPath: '../locales/add/{{lng}}/{{ns}}.missing.json',
+            addPath: '/locales/add/{{lng}}/{{ns}}.missing.json',
 
             // your backend server supports multiloading 
             // /locales/resources.json?lng=de+en&ns=ns1+ns2 
