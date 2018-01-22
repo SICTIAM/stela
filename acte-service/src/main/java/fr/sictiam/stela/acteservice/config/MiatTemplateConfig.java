1b50d24110f78a5d3c8ffb98662c419609935fb0
Raphaël de Rivas <raphderivas@gmail.com>
2018-01-22 11:30:04 +0100

    Fix certificate visibility

--- acte-service/src/main/java/fr/sictiam/stela/acteservice/config/MiatTemplateConfig.java
+++ acte-service/src/main/java/fr/sictiam/stela/acteservice/config/MiatTemplateConfig.java
@@ -1,6 +1,5 @@
 package fr.sictiam.stela.acteservice.config;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyManagementException;
@@ -21,6 +20,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -31,7 +31,14 @@ public class MiatTemplateConfig {
     
     @Value("${application.miat.truststorepasswd}")
     private String truststorePassword;
-
+    
+    @Value("${application.miat.truststorepath}")
+    private String trustStorePath;
+    
+    @Value("${application.miat.keystorepath}")
+    private String keyStorePath;
+    
+   
     @Bean
     @Qualifier("miatRestTemplate")
     public RestTemplate getRestTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException,
@@ -39,15 +46,12 @@ public class MiatTemplateConfig {
         TrustStrategy acceptingTrustStrategy = (x509Certificates, s) -> true;
 
         KeyStore keyStore = KeyStore.getInstance("PKCS12");
-        keyStore.load(
-                new FileInputStream(new File(
-                        MiatTemplateConfig.class.getResource("/certificates/stela.sictiam2015.fr.p12").getPath())),
+        keyStore.load(new FileInputStream(ResourceUtils.getFile(keyStorePath)),
                 keystorePassword.toCharArray());
         SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
                 .loadKeyMaterial(keyStore, keystorePassword.toCharArray())
-                .loadTrustMaterial(
-                        new File(MiatTemplateConfig.class.getResource("/certificates/truststore.jks").getPath()),
-                        truststorePassword.toCharArray(), acceptingTrustStrategy)
+                .loadTrustMaterial(ResourceUtils.getFile(trustStorePath), truststorePassword.toCharArray(),
+                        acceptingTrustStrategy)
                 .build();
         SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
         CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
