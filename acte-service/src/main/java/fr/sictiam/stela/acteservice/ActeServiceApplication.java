9070c30ebbfd4871b351823749c2e98da4b6d200
Raphaël de Rivas <raphderivas@gmail.com>
2018-04-23 10:33:01 +0200

    [Fix] set default time to central european time

--- acte-service/src/main/java/fr/sictiam/stela/acteservice/ActeServiceApplication.java
+++ acte-service/src/main/java/fr/sictiam/stela/acteservice/ActeServiceApplication.java
@@ -8,6 +8,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+
+import java.util.TimeZone;
+
 @EntityScan(basePackageClasses = { ActeServiceApplication.class })
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,6 +19,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @EnableRabbit
 public class ActeServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ActeServiceApplication.class, args);
     }
--- admin-service/src/main/java/fr/sictiam/stela/admin/AdminServiceApplication.java
+++ admin-service/src/main/java/fr/sictiam/stela/admin/AdminServiceApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import javax.annotation.PostConstruct;
+
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AdminServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("CET"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
