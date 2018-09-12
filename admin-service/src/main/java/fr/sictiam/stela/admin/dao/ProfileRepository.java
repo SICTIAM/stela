9857f65734087369ec19301253648ca9aad73506
f.boucher <f.boucher@sictiam.fr>
2018-09-12 14:16:36 +0200

    [Evo] ref #4642 - Order local authority list in top bar

--- admin-service/src/main/java/fr/sictiam/stela/admin/dao/ProfileRepository.java
+++ admin-service/src/main/java/fr/sictiam/stela/admin/dao/ProfileRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface ProfileRepository extends JpaRepository<Profile, String> {
     Optional<Profile> findByLocalAuthority_UuidAndAgent_Uuid(String localAuthorityUuid, String agentUuid);
@@ -12,6 +13,10 @@ public interface ProfileRepository extends JpaRepository<Profile, String> {
     Optional<Profile> findByUuid(String uuid);
 
     List<Profile> findByLocalAuthority_Uuid(String localAuthorityUuid);
+
+    Set<Profile> findByAgent_UuidOrderByLocalAuthority_name(String agentUuid);
+
+    Set<Profile> findByAgent_SubOrderByLocalAuthority_name(String sub);
 
     Optional<Profile> findByLocalAuthority_SirenAndAgent_Email(String siren, String email);
 }
