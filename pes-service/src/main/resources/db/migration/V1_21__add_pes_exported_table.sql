d5b204e04a020a61f3dacac038deb0acfab045c1
f.boucher <f.boucher@sictiam.fr>
2018-10-30 16:16:11 +0100

    Fix flyway

--- pes-service/src/main/resources/db/migration/V1_21__add_pes_exported_table.sql
+++ pes-service/src/main/resources/db/migration/V1_21__add_pes_exported_table.sql
@@ -1,10 +1,3 @@
-ALTER TABLE pes_history ADD COLUMN attachment_uuid character varying(255) NULL;
-ALTER TABLE pes_history ADD CONSTRAINT fk_attachment FOREIGN KEY (attachment_uuid) REFERENCES attachment(uuid);
-
-
-ALTER TABLE attachment ADD COLUMN storage_key character varying(255);
-ALTER TABLE pending_message DROP date, DROP file, DROP file_name, DROP message;
-
 CREATE TABLE pes_export (
     uuid character varying(255) NOT NULL,
     pes_uuid character varying(255) NOT NULL,
