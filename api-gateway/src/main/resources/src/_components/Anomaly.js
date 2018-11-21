2d7ee3f60439347b425b0292a24a5ca57e28a1b8
Julie ALBALADEJO <albaladejo.julie@gmail.com>
2018-11-21 10:07:23 +0100

    [Evo] #2484 - code review

--- api-gateway/src/main/resources/src/_components/Anomaly.js
+++ api-gateway/src/main/resources/src/_components/Anomaly.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { Message } from 'semantic-ui-react'
 
 import { anomalies } from '../_util/constants'
@@ -15,32 +15,19 @@ class Anomaly extends Component {
         type: 'negative'
     }
     render() {
-        const { header, lastHistory, type } = this.props
+        const { header, lastHistory, type = 'negative' } = this.props
         return (
-            <Fragment>
-                { anomalies.includes(lastHistory.status) && type === 'negative' && (
-                    <Message negative>
-                        <Message.Header style={{ marginBottom: '0.5em'}}>{header}</Message.Header>
-                        {lastHistory.message &&
-                            <p>{lastHistory.message}</p>
-                        }
-                        {lastHistory.errors &&
-                            <CollapsedList items={PesErrorList(lastHistory.errors, `anomaly-${lastHistory.uuid}`)}/>
-                        }
-                    </Message>
-                )}
-                { anomalies.includes(lastHistory.status) && type === 'warning' && (
-                    <Message warning>
-                        <Message.Header style={{ marginBottom: '0.5em'}}>{header}</Message.Header>
-                        {lastHistory.message &&
-                            <p>{lastHistory.message}</p>
-                        }
-                        {lastHistory.errors &&
-                            <CollapsedList items={PesErrorList(lastHistory.errors, `anomaly-${lastHistory.uuid}`)}/>
-                        }
-                    </Message>
-                )}
-            </Fragment>
+            anomalies.includes(lastHistory.status) && (
+                <Message negative={type === 'negative'} warning={type === 'warning'}>
+                    <Message.Header style={{ marginBottom: '0.5em'}}>{header}</Message.Header>
+                    {lastHistory.message &&
+                        <p>{lastHistory.message}</p>
+                    }
+                    {lastHistory.errors &&
+                        <CollapsedList items={PesErrorList(lastHistory.errors, `anomaly-${lastHistory.uuid}`)}/>
+                    }
+                </Message>
+            )
         )
     }
 }
