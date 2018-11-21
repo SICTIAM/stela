584684cdbf8c7a9859d59b871648b58d9ad36480
Gérald Golé <gerald.gole@gmail.com>
2018-11-21 11:43:54 +0100

    Log configuration

--- api-gateway/src/main/java/fr/sictiam/stela/apigateway/config/RequestLoggingFilterConfig.java
+++ api-gateway/src/main/java/fr/sictiam/stela/apigateway/config/RequestLoggingFilterConfig.java
@@ -1,4 +1,4 @@
-package fr.sictiam.stela.pesservice.config;
+package fr.sictiam.stela.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +9,14 @@ public class RequestLoggingFilterConfig {
 
     @Bean
     public CommonsRequestLoggingFilter logFilter() {
-        CommonsRequestLoggingFilter filter
-                = new CommonsRequestLoggingFilter();
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
         filter.setIncludeQueryString(true);
         filter.setIncludePayload(true);
         filter.setMaxPayloadLength(10000);
-        filter.setIncludeHeaders(false);
+        filter.setIncludeHeaders(true);
+        filter.setIncludeClientInfo(true);
+        filter.setAfterMessagePrefix("Incoming request : ");
+        filter.setAfterMessageSuffix("");
         return filter;
     }
 }
