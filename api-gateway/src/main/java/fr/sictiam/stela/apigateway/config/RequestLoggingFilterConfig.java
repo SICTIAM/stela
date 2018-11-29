8575b6d14d3684c3c8b0f4605b78a9202b00731f
Gérald Golé <gerald.gole@gmail.com>
2018-11-29 09:39:38 +0100

    Prints headers and parameters for incoming requests

--- api-gateway/src/main/java/fr/sictiam/stela/apigateway/config/RequestLoggingFilterConfig.java
+++ api-gateway/src/main/java/fr/sictiam/stela/apigateway/config/RequestLoggingFilterConfig.java
@@ -1,22 +1,56 @@
 package fr.sictiam.stela.apigateway.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RequestLoggingFilterConfig {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RequestLoggingFilterConfig.class);
+
     @Bean
     public CommonsRequestLoggingFilter logFilter() {
-        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
-        filter.setIncludeQueryString(true);
-        filter.setIncludePayload(true);
-        filter.setMaxPayloadLength(10000);
-        filter.setIncludeHeaders(true);
-        filter.setIncludeClientInfo(true);
-        filter.setAfterMessagePrefix("Incoming request : ");
-        filter.setAfterMessageSuffix("");
-        return filter;
+
+        return new CommonsRequestLoggingFilter() {
+
+            @Override protected void beforeRequest(HttpServletRequest request, String message) {
+
+                Map<String, Object> data = new HashMap<>();
+                String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() +
+                        request.getRequestURI() + (request.getQueryString() != null ? "?" + request.getQueryString() : "");
+
+                Map<String, String> headers = new HashMap<>();
+                Enumeration<String> headerNames = request.getHeaderNames();
+                while (headerNames.hasMoreElements()) {
+                    String name = headerNames.nextElement();
+                    headers.put(name, request.getHeader(name));
+                }
+                data.put("headers", headers);
+                data.put("parameters", request.getParameterMap());
+
+                ObjectMapper mapper = new ObjectMapper();
+                try {
+                    LOGGER.info("Incoming request : " + request.getMethod() + " " + url + " " + mapper.writeValueAsString(data));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            protected void afterRequest(HttpServletRequest request, String message) {
+            }
+        };
     }
 }
