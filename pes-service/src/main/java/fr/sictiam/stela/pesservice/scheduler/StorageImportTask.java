cc57b7589e385aec89dfba35ef4d4bc7df5a8e80
Gérald Golé <gerald.gole@gmail.com>
2019-01-07 09:39:12 +0100

    Remove transaction

--- pes-service/src/main/java/fr/sictiam/stela/pesservice/scheduler/StorageImportTask.java
+++ pes-service/src/main/java/fr/sictiam/stela/pesservice/scheduler/StorageImportTask.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -17,7 +16,6 @@ import javax.persistence.Query;
 
 import java.util.List;
 
-@Transactional
 @Component
 public class StorageImportTask {
 
