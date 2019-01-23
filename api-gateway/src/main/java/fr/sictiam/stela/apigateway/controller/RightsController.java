5e96a378b67a817225572ff13e766c61b1b961e7
Benoit Orihuela <borihuela@ozwillo.org>
2019-01-23 14:03:09 +0100

    feat: only call rights API for services registered in Eureka

--- api-gateway/src/main/java/fr/sictiam/stela/apigateway/controller/RightsController.java
+++ api-gateway/src/main/java/fr/sictiam/stela/apigateway/controller/RightsController.java
@@ -1,9 +1,9 @@
 package fr.sictiam.stela.apigateway.controller;
 
+import com.netflix.discovery.EurekaClient;
 import fr.sictiam.stela.apigateway.util.DiscoveryUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,38 +19,48 @@ public class RightsController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RightsController.class);
 
-    @Autowired
-    DiscoveryUtils discoveryUtils;
+    private final EurekaClient discoveryClient;
+
+    private final DiscoveryUtils discoveryUtils;
+
+    public RightsController(EurekaClient discoveryClient, DiscoveryUtils discoveryUtils) {
+        this.discoveryClient = discoveryClient;
+        this.discoveryUtils = discoveryUtils;
+    }
 
     @GetMapping
     public List<String> getRights() {
         RestTemplate restTemplate = new RestTemplate();
 
-        List<String> notificationList = new ArrayList<>();
+        List<String> rights = new ArrayList<>();
 
         try {
-            notificationList.addAll(Arrays.asList(restTemplate.getForObject(discoveryUtils.acteServiceUrl() + "/api/acte/rights",
-                    String[].class)));
+            if (discoveryClient.getApplication("acte-service") != null) {
+                rights.addAll(Arrays.asList(restTemplate.getForObject(discoveryUtils.acteServiceUrl() + "/api/acte/rights",
+                        String[].class)));
+            }
         } catch (RuntimeException e) {
             LOGGER.warn("Module acte is probably not running : {}", e.getMessage());
         }
 
         try {
-            notificationList.addAll(Arrays.asList(restTemplate.getForObject(discoveryUtils.convocationServiceUrl() +
-                            "/api/convocation/rights",
-                    String[].class)));
+            if (discoveryClient.getApplication("convocation-service") != null) {
+                rights.addAll(Arrays.asList(restTemplate.getForObject(discoveryUtils.convocationServiceUrl() +
+                                "/api/convocation/rights", String[].class)));
+            }
         } catch (RuntimeException e) {
             LOGGER.warn("Module convocation is probably not running : {}", e.getMessage());
         }
 
         try {
-            notificationList.addAll(Arrays.asList(restTemplate.getForObject(discoveryUtils.pesServiceUrl() + "/api" +
-                            "/pes/rights",
-                    String[].class)));
+            if (discoveryClient.getApplication("pes-service") != null) {
+                rights.addAll(Arrays.asList(restTemplate.getForObject(discoveryUtils.pesServiceUrl() + "/api" +
+                                "/pes/rights", String[].class)));
+            }
         } catch (RuntimeException e) {
             LOGGER.warn("Module pes is probably not running : {}", e.getMessage());
         }
 
-        return notificationList;
+        return rights;
     }
 }
