bdbaa5b211b00d31a813c7a970e7ec29483513e9
Benoit Orihuela <borihuela@ozwillo.org>
2019-02-12 18:53:23 +0100

    fix: add index when sending annexes to Pastell
    - and other minor comments and cleanup (sorry for that)

--- acte-service/src/main/java/fr/sictiam/stela/acteservice/scheduler/ArchiverTask.java
+++ acte-service/src/main/java/fr/sictiam/stela/acteservice/scheduler/ArchiverTask.java
@@ -1,15 +1,11 @@
 package fr.sictiam.stela.acteservice.scheduler;
 
 import fr.sictiam.stela.acteservice.service.ArchiverService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ArchiverTask {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ArchiverTask.class);
 
     private final ArchiverService archiverService;
 
