932b0fdf8a75ff9c203ae0572ee5e6e771e83d0a
Julie ALBALADEJO <albaladejo.julie@gmail.com>
2019-02-13 14:43:25 +0100

    feat: display responses additionnal questions
    feat issue: #2532

--- api-gateway/src/main/resources/src/convocation/Participants.js
+++ api-gateway/src/main/resources/src/convocation/Participants.js
@@ -1,25 +1,62 @@
-import React, { Component } from 'react'
-import { Tab} from 'semantic-ui-react'
+import React, { Component, Fragment } from 'react'
+import { Tab, Popup, Icon } from 'semantic-ui-react'
 import { translate } from 'react-i18next'
 
 class Participants extends Component {
 	createPresentContent = (answer) => {
+	    /** Create participant row with answer questions
+		 * First we filter one response (PRESENT)
+		 * Next for each participant we check if it is a answer for each questions -> create one <td></td> per questions
+		*/
 	    return this.props.participants.filter((part) => {
 	        return part.responseType === answer
 	    }).map(part => {
+	        const responses = this.props.questions.map((question) => {
+	            var indexQuestion = question.responses.findIndex((response) => {
+	                return response.recipient.uuid === part.recipient.uuid
+	            })
+	            if(indexQuestion !== -1) {
+    				return (
+	                    <Fragment key={`${part.recipient.uuid}_${question.uuid}`}>
+	                        {question.responses[indexQuestion].response === true && (
+	                            <td style={{textAlign: 'center', padding: '5px 0'}}>
+	                                <Icon style={{color: '#419443'}} name='check'/>
+	                            </td>
+	                        )}
+	                        {question.responses[indexQuestion].response === false && (
+	                            <td style={{textAlign: 'center', padding: '5px 0'}}>
+	                                <Icon style={{color: '#c73f3f'}} name='cancel'/>
+	                            </td>
+	                        )}
+	                    </Fragment>
+	                )
+	            } else {
+	                return (
+					    <td></td>
+	                )
+	            }
+	        })
 	        return (
 	            <tr key={`participant_${part.recipient.uuid}`}>
 	                <td key={`info_participant_${part.recipient.uuid}`}>{`${part.recipient.firstname} ${part.recipient.lastname}`}</td>
+	                {responses}
 	            </tr>
 	        )
 	    })
 	}
 	render() {
+	    const questions = this.props.questions.map((question) => {
+	        return (
+	            <Popup key={question.uuid} trigger={<th className='text-ellipsis mw-200 cursor-default' id={question.uuid}>{question.question}</th>}  content={question.question}/>
+	        )
+	    })
+
 	    const presentContent =
 			<table style={{borderSpacing: '10px'}}>
 			    <thead>
 			        <tr>
 			            <th></th>
+			            {questions}
 			        </tr>
 			    </thead>
 			    <tbody>
