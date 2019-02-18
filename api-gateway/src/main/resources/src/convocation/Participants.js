c3f7b0f5e2c704a9cff3ec9ec75ad7042573fea4
Julie ALBALADEJO <albaladejo.julie@gmail.com>
2019-02-18 15:14:29 +0100

    feat: code review
    feat issue: #2532

--- api-gateway/src/main/resources/src/convocation/Participants.js
+++ api-gateway/src/main/resources/src/convocation/Participants.js
@@ -1,45 +1,73 @@
 import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
 import { Tab, Popup, Icon } from 'semantic-ui-react'
 import { translate } from 'react-i18next'
 
 class Participants extends Component {
-	createPresentContent = (answer) => {
-	    /** Create participant row with answer questions
-		 * First we filter one response (PRESENT)
-		 * Next for each participant we check if it is a answer for each questions -> create one <td></td> per questions
-		*/
+	static propTypes = {
+	    questions: PropTypes.array.isRequired,
+	    participants: PropTypes.array.isRequired,
+	}
+	/**
+	 * @param {string} answer - answer we want: 'PRESENT', 'NOT_PRESENT' or 'SUBSTITUTION'
+	 * @return {array}
+	 * @description return the participants who answered PRESENT
+	 */
+	filterRecipientsByResponses = (answer) => {
 	    return this.props.participants.filter((part) => {
 	        return part.responseType === answer
-	    }).map(part => {
-	        const responses = this.props.questions.map((question) => {
-	            var indexQuestion = question.responses.findIndex((response) => {
-	                return response.recipient.uuid === part.recipient.uuid
-	            })
-	            if(indexQuestion !== -1) {
-    				return (
-	                    <Fragment key={`${part.recipient.uuid}_${question.uuid}`}>
-	                        {question.responses[indexQuestion].response === true && (
-	                            <td style={{textAlign: 'center', padding: '5px 0'}}>
-	                                <Icon style={{color: '#419443'}} name='check'/>
-	                            </td>
-	                        )}
-	                        {question.responses[indexQuestion].response === false && (
-	                            <td style={{textAlign: 'center', padding: '5px 0'}}>
-	                                <Icon style={{color: '#c73f3f'}} name='cancel'/>
-	                            </td>
-	                        )}
-	                    </Fragment>
-	                )
-	            } else {
-	                return (
-					    <td></td>
-	                )
-	            }
-	        })
+	    })
+	}
+	/**
+	 * @param {object} part - object contains participant response:
+	 * exemple: {"uuid":"","recipient":{"uuid":"","firstname":"Julie","lastname":"","email":"","phoneNumber":"","active":true/false,"additionalContact":true/false,"inactivityDate":null}
+	 * @param {object} question - question object with responses for each participant
+	 * @return {number}
+	 */
+	getIndexResponseParticipantByQuestion = (part, question) => {
+	    return question.responses.findIndex((response) => {
+	        return response.recipient.uuid === part.recipient.uuid
+	    })
+	}
+
+	/**
+	 * @param {object} part - object contains participant responses
+	 * @return {array}
+	 * @description return array with HTML for participants responses
+	 */
+	getResponsesByParticipant = (part) => {
+	    return this.props.questions.map((question) => {
+	        const indexQuestion = this.getIndexResponseParticipantByQuestion(part, question)
+	        if(indexQuestion !== -1) {
+	            return (
+	                <Fragment key={`${part.recipient.uuid}_${question.uuid}`}>
+	                    {question.responses[indexQuestion].response === true && (
+	                        <td style={{textAlign: 'center', padding: '5px 0'}}>
+	                            <Icon style={{color: '#419443'}} name='check'/>
+	                        </td>
+	                    )}
+	                    {question.responses[indexQuestion].response === false && (
+	                        <td style={{textAlign: 'center', padding: '5px 0'}}>
+	                            <Icon style={{color: '#c73f3f'}} name='cancel'/>
+	                        </td>
+	                    )}
+	                </Fragment>
+	            )
+	        } else {
+	            return (
+	                <td/>
+	            )
+	        }
+	    })
+	}
+
+	createPresentContent = (answer) => {
+	    return this.filterRecipientsByResponses(answer).map(part => {
+	        const responseHTML = this.getResponsesByParticipant(part)
 	        return (
 	            <tr key={`participant_${part.recipient.uuid}`}>
 	                <td key={`info_participant_${part.recipient.uuid}`}>{`${part.recipient.firstname} ${part.recipient.lastname}`}</td>
-	                {responses}
+	                {responseHTML}
 	            </tr>
 	        )
 	    })
@@ -64,9 +92,7 @@ class Participants extends Component {
 			    </tbody>
 			</table>
 
-	    const absent = this.props.participants.filter(part => {
-	        return part.responseType === 'NOT_PRESENT'
-	    }).map(part => {
+	    const absent = this.filterRecipientsByResponses('NOT_PRESENT').map(part => {
 	        return (
 	            <p key={part.recipient.uuid}>
 	                {`${part.recipient.firstname} ${part.recipient.lastname}`}
