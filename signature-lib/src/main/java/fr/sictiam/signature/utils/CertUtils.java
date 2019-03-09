e68e93b268608ade4406fd7d0624aa3073c49139
Benoit Orihuela <borihuela@ozwillo.org>
2019-03-09 16:31:06 +0100

    chore: cleanup in used certs and CAs

--- signature-lib/src/main/java/fr/sictiam/signature/utils/CertUtils.java
+++ signature-lib/src/main/java/fr/sictiam/signature/utils/CertUtils.java
@@ -7,18 +7,13 @@ import eu.europa.esig.dss.client.http.DataLoader;
 import eu.europa.esig.dss.client.http.commons.CommonsDataLoader;
 import eu.europa.esig.dss.client.http.commons.OCSPDataLoader;
 import eu.europa.esig.dss.client.ocsp.OnlineOCSPSource;
-import eu.europa.esig.dss.tsl.TrustedListsCertificateSource;
-import eu.europa.esig.dss.tsl.service.TSLRepository;
-import eu.europa.esig.dss.tsl.service.TSLValidationJob;
 import eu.europa.esig.dss.validation.CertificateValidator;
 import eu.europa.esig.dss.validation.CertificateVerifier;
 import eu.europa.esig.dss.validation.CommonCertificateVerifier;
 import eu.europa.esig.dss.validation.policy.rules.Indication;
 import eu.europa.esig.dss.validation.reports.CertificateReports;
-import eu.europa.esig.dss.validation.reports.DetailedReport;
 import eu.europa.esig.dss.x509.CertificateToken;
 import eu.europa.esig.dss.x509.CommonTrustedCertificateSource;
-import eu.europa.esig.dss.x509.KeyStoreCertificateSource;
 import fr.sictiam.signature.pes.CertificateContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,10 +25,8 @@ import java.io.InputStream;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class CertUtils {
 
@@ -62,33 +55,6 @@ public class CertUtils {
             }
         }
         return indicationResult;
-    }
-
-    public static TrustedListsCertificateSource getTrustedListsCertificateSource() throws IOException {
-        TrustedListsCertificateSource certificateSource = new TrustedListsCertificateSource();
-
-        // FIXME: signature/keystore.p12 must be in the ressources directory of the module using this lib
-        KeyStoreCertificateSource keyStoreCertificateSource = new KeyStoreCertificateSource(
-                new ClassPathResource("signature/keystore.p12").getInputStream(), "PKCS12", "dss-password");
-
-        TSLRepository tslRepository = new TSLRepository();
-        tslRepository.setTrustedListsCertificateSource(certificateSource);
-
-        TSLValidationJob job = new TSLValidationJob();
-        job.setDataLoader(dataloader);
-        job.setRepository(tslRepository);
-        job.setLotlUrl("https://ec.europa.eu/information_society/policy/esignature/trusted-list/tl-mp.xml");
-        job.setLotlRootSchemeInfoUri("https://ec.europa.eu/information_society/policy/esignature/trusted-list/tl.html");
-        job.setLotlCode("FR");
-        job.setFilterTerritories(Collections.singletonList("FR"));
-        job.setOjUrl("http://eur-lex.europa.eu/legal-content/EN/TXT/?uri=uriserv:OJ.C_.2016.233.01.0001.01.ENG");
-        job.setOjContentKeyStore(keyStoreCertificateSource);
-        job.setCheckLOTLSignature(true);
-        job.setCheckTSLSignatures(true);
-
-        job.refresh();
-
-        return certificateSource;
     }
 
     public static CertificateVerifier getCertificateVerifier() throws IOException, CertificateException {
@@ -129,18 +95,6 @@ public class CertUtils {
             throw e;
         }
         return null;
-    }
-
-    public static boolean isSigned(DetailedReport report) {
-        return !report.getSignatureIds().isEmpty();
-    }
-
-    public static List<SignatureResult> getSignatureResults(DetailedReport report) {
-        return report.getSignatureIds().stream()
-                .map(signatureId -> new SignatureResult(signatureId,
-                        report.getBasicValidationIndication(signatureId),
-                        report.getBasicValidationSubIndication(signatureId)))
-                .collect(Collectors.toList());
     }
 
     public static X509Certificate getCertificateFromBytes(byte[] bytes) throws CertificateException {
