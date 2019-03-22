e2252cc208878bb0ff64127b78c1879e3f7531a3
Julie ALBALADEJO <albaladejo.julie@gmail.com>
2019-03-22 09:18:13 +0100

    feat: fix test object is equivalent
    feat issue: #

--- api-gateway/src/main/resources/src/_util/utils.js
+++ api-gateway/src/main/resources/src/_util/utils.js
@@ -236,22 +236,24 @@ const extractFieldNameFromId = (str) => {
 
 const objectsIsEquivalent = (obj1, obj2) => {
     //Loop through properties in object 1
-    for (let p in obj1) {
-        //Check property exists on both objects
-        if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false
+    if(typeof (obj1) === 'object') {
+        for (let p in obj1) {
+            //Check property exists on both objects
+            if (typeof (obj2) !== 'object' || obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false
 
-        switch (typeof (obj1[p])) {
-        //Deep compare objects
-        case 'object':
-            if (!objectsIsEquivalent(obj1[p], obj2[p])) return false
-            break
-            //Compare function code
-        case 'function':
-            if (typeof (obj2[p]) === 'undefined' || (p !== 'compare' && obj1[p].toString() !== obj2[p].toString())) return false
-            break
-            //Compare values
-        default:
-            if (obj1[p] !== obj2[p]) return false
+            switch (typeof (obj1[p])) {
+            //Deep compare objects
+            case 'object':
+                if (typeof (obj2) === 'object' && !objectsIsEquivalent(obj1[p], obj2[p])) return false
+                break
+                //Compare function code
+            case 'function':
+                if (typeof (obj2[p]) === 'undefined' || (p !== 'compare' && obj1[p].toString() !== obj2[p].toString())) return false
+                break
+                //Compare values
+            default:
+                if (obj1[p] !== obj2[p]) return false
+            }
         }
     }
 
