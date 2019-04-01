30e62142342cc2611c52473cc11320616a272548
Boulajine <a.boulajine@sictiam.fr>
2019-04-01 09:01:09 +0200

    feat: call only active business services #5890

--- api-gateway/src/test/java/fr/sictiam/stela/apigateway/service/RightsServiceTest.java
+++ api-gateway/src/test/java/fr/sictiam/stela/apigateway/service/RightsServiceTest.java
@@ -2,24 +2,33 @@ package fr.sictiam.stela.apigateway.service;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
 import fr.sictiam.stela.apigateway.util.DiscoveryUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.util.ReflectionTestUtils;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.mockito.ArgumentMatchers.matches;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = RightsService.class)
@@ -34,6 +43,18 @@ public class RightsServiceTest {
 
     @MockBean
     private DiscoveryUtils discoveryUtils;
+
+    @MockBean
+    private EurekaClient discoveryClient;
+
+    @Mock
+    private InstanceInfo instanceInfo;
+
+    @Mock
+    private InstanceInfo instanceInfo1;
+
+    @Mock
+    private Application application;
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(8089);
@@ -53,31 +74,88 @@ public class RightsServiceTest {
                         .withHeader("Content-Type", "application/json")));
 
 
-        given(modulesService.getModules()).willReturn(Arrays.asList("convocation", "pes", "acte"));
-
         given(discoveryUtils.getServiceUrlByName(matches("(acte|pes|convocation)"))).willReturn("http://localhost:8089");
+        given(modulesService.extractServiceName("acte-service")).willReturn("acte");
+        given(modulesService.extractServiceName("pes-service")).willReturn("pes");
+        given(modulesService.extractServiceName("convocation-service")).willReturn("convocation");
     }
 
     @Test
     public void getRights() {
-        given(modulesService.moduleHaveInstance(matches("(acte|pes|convocation)"))).willReturn(true);
+        InstanceInfo instanceInfoActe = this.instanceInfo("acte", "acte-service", "BUSINESS");
+        InstanceInfo instanceInfoPes = this.instanceInfo("pes", "pes-service", "BUSINESS");
+        InstanceInfo instanceInfoConvocation = this.instanceInfo("convocation", "convocation-service", "BUSINESS");
+
+        Application applicationActe = this.application(instanceInfoActe, "acte-service");
+        Application applicationPes = this.application(instanceInfoPes, "pes-service");
+        Application applicationConvocation = this.application(instanceInfoConvocation, "convocation-service");
+
+
+        List<Application> applications = new ArrayList<>();
+
+        applications.add(applicationActe);
+        applications.add(applicationPes);
+        applications.add(applicationConvocation);
+
+        given(modulesService.activeBusinessApplications()).willReturn(applications);
 
         List<String> rights = rightsService.getRights();
-
-        WireMock.verify(3, getRequestedFor(urlPathMatching("/api/(acte|pes|convocation)/rights")));
 
         Assert.assertEquals(6, rights.size());
     }
 
     @Test
     public void getRightsWithoutPESModuleInstance() {
-        given(modulesService.moduleHaveInstance(matches("(acte|convocation)"))).willReturn(true);
-        given(modulesService.moduleHaveInstance(matches("pes"))).willReturn(false);
+        InstanceInfo instanceInfoActe = this.instanceInfo("acte", "acte-service", "BUSINESS");
+        InstanceInfo instanceInfoConvocation = this.instanceInfo("convocation", "convocation-service", "BUSINESS");
+
+        Application applicationActe = this.application(instanceInfoActe, "acte-service");
+        Application applicationConvocation = this.application(instanceInfoConvocation, "convocation-service");
+
+
+        List<Application> applications = new ArrayList<>();
+
+        applications.add(applicationActe);
+        applications.add(applicationConvocation);
+
+        given(modulesService.activeBusinessApplications()).willReturn(applications);
 
         List<String> rights = rightsService.getRights();
 
         WireMock.verify(2, getRequestedFor(urlPathMatching("/api/(acte|convocation)/rights")));
 
         Assert.assertEquals(5, rights.size());
+    }
+
+    private InstanceInfo instanceInfo(String name, String serviceName, String appGroupName) {
+        InstanceInfo instanceInfo = mock(InstanceInfo.class);
+        Map<String, String> metadata = new ConcurrentHashMap<>();
+        metadata.put("name", name);
+        ReflectionTestUtils.setField(instanceInfo, "instanceId", serviceName);
+        ReflectionTestUtils.setField(instanceInfo, "appName", serviceName);
+        ReflectionTestUtils.setField(instanceInfo, "appGroupName", appGroupName);
+        ReflectionTestUtils.setField(instanceInfo, "homePageUrl", "http://localhost:8089");
+        ReflectionTestUtils.setField(instanceInfo, "metadata", metadata);
+
+        return instanceInfo;
+    }
+
+    private Application application(InstanceInfo instanceInfo, String name) {
+        Application application = new Application();
+
+        List<InstanceInfo> instanceInfosList = new ArrayList<>();
+        instanceInfosList.add(instanceInfo);
+
+        AtomicReference<List<InstanceInfo>> shuffledInstances = new AtomicReference<>();
+        shuffledInstances.set(instanceInfosList);
+
+        Map<String, InstanceInfo> instancesMap = new ConcurrentHashMap<>();
+        instancesMap.put(name, instanceInfo);
+
+        ReflectionTestUtils.setField(application, "name", name);
+        ReflectionTestUtils.setField(application, "shuffledInstances", shuffledInstances);
+        ReflectionTestUtils.setField(application, "instancesMap", instancesMap);
+
+        return application;
     }
 }
